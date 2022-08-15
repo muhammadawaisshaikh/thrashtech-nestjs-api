f38eeb34be3c1a8243dcc257b47cc84a74ace27c
Muhammad Awais <awaisshaikh94@gmail.com>
2022-08-16 02:12:31 +0500

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Thrashtech')
+    .setDescription('Thrashtech API for managing tanker loads and release load with other features')
+    .setVersion('1.0')
+    .addTag('thrashtech-api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
