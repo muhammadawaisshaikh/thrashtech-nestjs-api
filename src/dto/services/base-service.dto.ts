a04846be601b5b4ec8439bffe4997bf683553cf3
Muhammad Awais <awaisshaikh94@gmail.com>
2022-08-16 09:35:34 +0500

    documenting swagger details for services controller

--- src/dto/services/base-service.dto.ts
+++ src/dto/services/base-service.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class BaseService {
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   description?: string;
+
+  @ApiProperty()
   image?: string;
 }
--- src/modules/services/services.controller.ts
+++ src/modules/services/services.controller.ts
@@ -7,7 +7,12 @@ import {
     Post,
     Put,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+    ApiBearerAuth,
+    ApiOperation,
+    ApiResponse,
+    ApiTags,
+} from '@nestjs/swagger';
 import { BaseService } from 'src/dto/services/base-service.dto';
 import { ServicesService } from './services.service';
 
@@ -17,26 +22,56 @@ export class ServicesController {
     constructor(private readonly service: ServicesService) { }
 
     @Get()
+    @ApiOperation({ summary: 'Get List of Services' })
+    @ApiResponse({
+        status: 200,
+        description: 'The records found',
+        type: [BaseService],
+    })
     async index() {
         return await this.service.findAll();
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Get Service' })
+    @ApiResponse({
+        status: 200,
+        description: 'The record found',
+        type: BaseService,
+    })
     async find(@Param('id') id: string) {
         return await this.service.findOne(id);
     }
 
     @Post()
+    @ApiOperation({ summary: 'Create Service' })
+    @ApiResponse({
+        status: 200,
+        description: 'The record found',
+        type: BaseService,
+    })
     async create(@Body() createServiceDto: BaseService) {
         return await this.service.create(createServiceDto);
     }
 
     @Put(':id')
+    @ApiOperation({ summary: 'Update Service' })
+    @ApiResponse({
+        status: 200,
+        description: 'The record updated',
+        type: BaseService,
+    })
     async update(@Param('id') id: string, @Body() updateServiceDto: BaseService) {
         return await this.service.update(id, updateServiceDto);
     }
 
     @Delete(':id')
+    @ApiOperation({ summary: 'Delete Service' })
+    @ApiResponse({
+        status: 200,
+        description: 'The record deleted',
+        type: BaseService,
+    })
     async delete(@Param('id') id: string) {
         return await this.service.delete(id);
     }
